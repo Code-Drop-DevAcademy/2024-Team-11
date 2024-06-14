dd4589f5d5f0b08f3c94876c8eb186a2e7501336
Hyun Lee <hyunlee@10-141-41-42.0101.pos.kr.iosda.org>
2024-06-15 02:21:08 +0900

    Init: Xcode Project 생성

---
+++ Code Drop_Team 11/Code Drop_Team 11/Code_Drop_Team_11App.swift
@@ -0,0 +1,17 @@
+//
+//  Code_Drop_Team_11App.swift
+//  Code Drop_Team 11
+//
+//  Created by Hyun Lee on 6/15/24.
+//
+
+import SwiftUI
+
+@main
+struct Code_Drop_Team_11App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
